3400aa2fce3171ae95c9ca8c7351ee6671006971
Александр Жуков <alexrabota44@mail.ru>
2025-02-26 22:57:16 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,56 @@
+/*  ДЗ 1) */
+
+let userName = "Александр";
+let userAge = 40;
+let pet = false;
+let favoriteBook = null;
+
+
+
+/* ДЗ 2) */
+
+"JavaScript" = typeof (String);
+2024 = typeof (Number);
+true = typeof (Boolean);
+null = typeof (null) (отсутствие значения);
+undefined = typeof (Undefined) (значение для необьявленых переменных);
+
+/* ДЗ 3) */
+
+let user = {name:"Alice", age:30};
+console.log(typeof user);
+
+ответ ( object )
+
+
+functionsauHello() { console.log("Hello!");}
+console.log(typeof sayHello);
+
+ответ ( SyntaxError: Unexpected token '{' )
+
+/* если исправить имя на одинаковое то: */
+
+function sauHello() { console.log("Hello!");}
+console.log(typeof sayHello);
+
+ответ ( undefined )
+
+
+let combined = "Age:" + 25; 
+console.log(typeof combined);
+
+ответ ( string )
+
+
+/* ДЗ 3) */
+
+let user = {
+    firstName = "Александр",
+    lastName = "Жуков",
+    age = 40,
+    isEmployed = true,
+    contact = "mail@email",
+    phone = undefined, 
+};
+const uniqueId = Sumbol('id');
+user uniqueId = 13;
