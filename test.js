bfa5af3b16c2f54cef188c5f2eb388c9572d60bc
Александр Жуков <alexrabota44@mail.ru>
2025-03-10 22:50:27 +0300

    урок 11

--- test.js
+++ test.js
@@ -341,7 +341,7 @@ let changeTextObj = document.querySelector(".obj"); // берем класс ( o
 const values = Object.values(obj);                  // берем значение обьекта ( obj )
 changeTextObj.innerText = values.join(", ");        // выводим текстом обьединяем чере ( , )
 
-*/
+
 
 // урок № 10
 
@@ -387,4 +387,51 @@ let outputKey = document.getElementById("outputKey");
 
 document.addEventListener("keydown", function(event) {  // event это событие 
     outputKey.textContent = "вы нажали: " + event.key;  // отоброжение события
+});
+
+*/
+
+// упрок № 11
+
+
+// массив данных 
+const user =[
+    { name: "Анна", age: 25 },
+    { name: "Михаил", age: 15 },
+    { name: "Иван", age: 16},
+    { name: "Мария", age: 18},
+    { name: "Дмитрий", age: 12},
+    { name: "София", age: 20}
+];
+
+// получение элементов из DOM 
+let showAdultsButton = document.getElementById("showAdults");
+
+let showChildrenButton = document.getElementById("showChildren");
+
+let userList = document.getElementById("userList");
+
+// функция для получения списка людей 
+
+function showUsers(userArray) {
+    userList.innerHTML = " ";
+    userArray.forEach( user => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${user.name}, ${user.age} лет`;
+        userList.appendChild(listItem);
+    });
+};
+
+// обработчик события вывода взрослых 
+
+showAdultsButton.addEventListener("click", () => {
+    const adults = user.filter(user => user.age >= 18);
+    showUsers(adults);
+});
+
+// обработчик события вывода детей 
+
+showChildrenButton.addEventListener("click", () => {
+    const children = user.filter(user => user.age < 18);
+    showUsers(children);
 });
