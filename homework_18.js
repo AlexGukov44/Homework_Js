1bae3fd456ea7abc8ed8dba37b332de9e6361f64
Александр Жуков <alexrabota44@mail.ru>
2025-03-23 19:51:57 +0300

    дз 18v4

--- homework_18.js
+++ homework_18.js
@@ -5,33 +5,33 @@ fetch('https://randomuser.me/api/?results=10')
     .then(function(data) {
         let userBox = document.getElementById('user');
         userBox.innerHTML = `
-        <img src="URL"${(data.results[0].picture.large)}>
-        <p>${(data.results[0].name.first + " " + data.results[0].name.last)}</p>
-        <p>${(data.results[0].gender)}</p>
-        <p>${(data.results[0].location.city)}</p>
-        <a href="mailto:(${(data.results[0].email)})"></a>
-        <p>${(data.results[0].login.username)}</p>
-        <p>${(data.results[0].login.password)}</p>
+        <img src="${(data.results[0].picture.large)}" alt="фото">
+        <p>Имя: ${(data.results[0].name.first + " " + data.results[0].name.last)}</p>
+        <p>Пол: ${(data.results[0].gender)}</p>
+        <p>Город: ${(data.results[0].location.city)}</p>
+        <a href="mailto:${data.results[0].email}">Email: ${(data.results[0].email)}</a>
+        <p>Логин: ${(data.results[0].login.username)}</p>
+        <p>Пароль: ${(data.results[0].login.password)}</p>
         `;
         if (data.results && data.results.length > 0) {
             resultsBox = document.createElement('div');
-            resultsBox.classList.add(''); // добавить классы после того как заработает
-            resultsBox.innerHTML = `<h2>ещё людей )))</h2>`
+            resultsBox.classList.add('other-users'); // добавить классы после того как заработает
+            resultsBox.innerHTML = `<h2>ещё люди:</h2>`
             userBox.appendChild(resultsBox);
 
-            data.results.forEach(function(otherUser, index) {
+            data.results.forEach((user, index) => {
                 let otherBox = document.createElement('div');
-                otherBox.classList.add(''); // добавить классы после того как заработает
+                otherBox.classList.add('user-item'); // добавить классы после того как заработает
                 otherBox.innerHTML = `
                 <div>${(index + 1)} человек</div>
-                <img src="URL"${(otherUser.results[index].picture.large)}>
-                <p>${(otherUser.results[index].name.first +  " " + otherUser.name.last)}</p>
-                <p>${(otherUser.results[index].gender)}</p>
-                <p>${(otherUser.results[index].location.city)}</p>
-                <a href="mailto:(${(otherUser.results[index].email)})"></a>
-                <p>${(otherUser.results[index].login.username)}</p>
-                <p>${(otherUser.results[index].login.password)}</p>
-                `;
+                <img src="${(user.picture.large)}" alt="фото">
+                <p>Имя: ${(user.name.first +  " " + user.name.last)}</p>
+                <p>Пол: ${(user.gender)}</p>
+                <p>Город: ${(user.location.city)}</p>
+                <a href="mailto:${data.results[0].email}">Email: ${(data.results[0].email)}></a>
+                <p>Логин: ${(user.login.username)}</p>
+                <p>Пароль: ${(user.login.password)}</p>
+                `; 
                 resultsBox.appendChild(otherBox);
             })
         }
