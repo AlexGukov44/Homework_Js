52403b82ba7e7e17b6669f77b42dc3225ca42730
Александр Жуков <alexrabota44@mail.ru>
2025-03-24 18:14:01 +0300

    18 itog

--- homework_18.js
+++ homework_18.js
@@ -9,7 +9,7 @@ fetch('https://randomuser.me/api/?results=10')
         <p>Имя: ${(data.results[0].name.first + " " + data.results[0].name.last)}</p>
         <p>Пол: ${(data.results[0].gender)}</p>
         <p>Город: ${(data.results[0].location.city)}</p>
-        <a href="mailto:${data.results[0].email}">Email: ${(data.results[0].email)}</a>
+        <p>Email: <a href="mailto:${data.results[0].email}">${(data.results[0].email)}</a></p>
         <p>Логин: ${(data.results[0].login.username)}</p>
         <p>Пароль: ${(data.results[0].login.password)}</p>
         `;
@@ -19,7 +19,7 @@ fetch('https://randomuser.me/api/?results=10')
             resultsBox.innerHTML = `<h2>ещё люди:</h2>`
             userBox.appendChild(resultsBox);
 
-            data.results.forEach((user, index) => {
+            data.results.slice(1).forEach((user, index) => {
                 let otherBox = document.createElement('div');
                 otherBox.classList.add('user-item'); // добавить классы после того как заработает
                 otherBox.innerHTML = `
@@ -28,7 +28,7 @@ fetch('https://randomuser.me/api/?results=10')
                 <p>Имя: ${(user.name.first +  " " + user.name.last)}</p>
                 <p>Пол: ${(user.gender)}</p>
                 <p>Город: ${(user.location.city)}</p>
-                <a href="mailto:${data.results[0].email}">Email: ${(data.results[0].email)}></a>
+                <p>Email: <a href="mailto:${data.results[0].email}">${(data.results[0].email)}</a></p>
                 <p>Логин: ${(user.login.username)}</p>
                 <p>Пароль: ${(user.login.password)}</p>
                 `; 
