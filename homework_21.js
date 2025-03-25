232f9ff3ba7f7fc26c2740fbb2bbc6c64f563c47
Александр Жуков <alexrabota44@mail.ru>
2025-03-25 22:06:13 +0300

    дз 21

--- homework_21.js
+++ homework_21.js
@@ -1,10 +1,13 @@
+
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(MotionPathPlugin)
-    gsap.to('#circle', { x:650, y:650, duration: 7 })
-        .then(() => gsap.to('#circle', { scale: 2, duration: 2 }));
-    gsap.to('#circle', { x: 350, y: 350, backgroundColor: "blue", duration: 3 });
+    gsap.to('#circle', { x:650, y:650, duration: 4, backgroundColor: "blue",
+        onComplete: () => {
+            gsap.to('#circle', { scale: 2, duration: 2 });
+        }
+     });
     gsap.to('#triangle', { x: 650, duration: 8 });
-    gsap.to('#box', { rotation: 360, duration: 2 });
-    gsap.to('#circle2', { scale: 2, duration: 5 })
-        .then(() => gsap.to('#circle2', { y:100, duration: 3 }));
-});
+    gsap.to('#box', { rotation: 360, duration: 8 });
+    gsap.to('#circle2', { scale: 2, duration: 6 })
+        .then(() => gsap.to('#circle2', { y:100, duration: 2 }));
+}); 
