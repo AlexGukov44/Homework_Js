961dfe3727d2f0d81d57b6b6b59d07890fd9bc93
Александр Жуков <alexrabota44@mail.ru>
2025-03-30 22:32:49 +0300

    дз 25

--- homework_25.js
+++ homework_25.js
@@ -11,47 +11,40 @@ let scale = 100;
 document.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'ArrowUp':
-            y -= 10;
+            if (y > 0) y -= 10;
             break;
         case 'ArrowDown':
-            y += 10;
+            if (y < window.innerHeight - objectHeight) y += 10;
             break;
         case 'ArrowLeft':
-            x -= 10;
+            if (x >0) x -= 10;
             break;
         case 'ArrowRight':
-            x += 10;
+            if (x < window.innerWidth - objectWidth) x += 10;
             break;
         case 'l':
             rotation += 180;
             break;
         case 'k':
-            scale += 10;
+            scale += 100;
             updateObjectStyle();
             setTimeout(() => {
                 scale = 100;
+                updateObjectStyle();
             }, 2000);
             break;
     }
-    x = Math.max(0, Math.min(x, window.innerWidth - objectWidth));
-    y = Math.max(0, Math.min(y, window.innerHeight - objectHeight));
+    updateObjectStyle();
+});
 
+function updateObjectStyle() {
     object.style.transform = `translate(${x}px, ${y}px) rotate(${rotation}deg) scale(${scale/100})`;
-
     let count = document.getElementById('count');
     count.innerHTML = `
     <div>x = ${x}</div>
     <div>y = ${y}</div>
     `;
-});
-
-// object.addEventListener('mousedown', () => {
-//     object.style.opacity = 0;
-
-//     setTimeout(() => {
-//         object.style.opacity = 1;
-//     }, 2000);
-// });
+}
 
 object.addEventListener('mousedown', () => {
     object.classList.add('grabbed');
