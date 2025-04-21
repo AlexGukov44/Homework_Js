9f425526f63faf1b251a9f658d5ed8a1c73e65ee
Александр Жуков <alexrabota44@mail.ru>
2025-04-21 19:12:20 +0300

    ДЗ 30м2 ?

---
+++ homework_30v2.js
@@ -0,0 +1,30 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 10;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Подключите текстуры
+
+const material = new THREE.MeshBasicMaterial( { map:texture } );
+const texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/timoxley/threejs/master/examples/textures/land_ocean_ice_cloud_2048.jpg' ); 
+const geometry = new THREE.SphereGeometry(3, 32, 32);
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    sphere.rotation.y += 0.005;
+    renderer.render(scene, camera);
+}
+
+animate();
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
